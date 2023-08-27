350f6a80bd963611c3fe7359adc9c3f031580628
Feri Hermawan <ferihermawan007@gmail.com>
2023-08-27 08:30:48 +0700

    mengubah int age ke string

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,9 @@
 package sayhello
 
+import (
+	"strconv"
+)
+
 func SayHello(name string, age int) string {
-	return "Hello my name is " + name + "and my age is " + (string(age))
+	return "Hello my name is " + name + "and my age is " + strconv.Itoa(age)
 }
